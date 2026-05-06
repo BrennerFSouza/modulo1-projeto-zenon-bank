b2b00c263925bed05c8b7c1fd7387d674061c776
Brenner Souza <brenner.souza@gmail.com>
2026-05-06 14:44:31 -0300

    Formatando moeda

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -1,13 +1,16 @@
 package br.com.zenon.fraud;
 
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
 
 public class ReportMain {
     static void main() {
-        Locale locale = Locale.of("pt-BR");
+        Locale locale = Locale.of("pt", "br");
         var integgerFormatter = NumberFormat.getIntegerInstance(locale);
+        var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        currencyFormatter.setCurrency(Currency.getInstance("USD"));
 
 
         String arquivo = "PS_20174392719_1491204439457_log.csv";
@@ -20,10 +23,11 @@ public class ReportMain {
         TransactionReport.Statistics statistics = transactionReport.generateReport(arquivo);
         String formattedTotalTransactions = integgerFormatter.format(statistics.totalTransactions());
         String formattedTotalFrauds = integgerFormatter.format(statistics.totalFrauds());
+        String formattedTotalAmount = currencyFormatter.format(statistics.totalAmount());
 
 
         System.out.println("Total de linhas: " + formattedTotalTransactions);
         System.out.println("Total de fraudes: " + formattedTotalFrauds);
-        System.out.println("Valor total transacionado: " + statistics.totalAmount().toPlainString());
+        System.out.println("Valor total transacionado: "    + formattedTotalAmount);
     }
 }
